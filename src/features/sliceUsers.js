98cb772ffa1a850aab97620b0e635106d5d12b33
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-12-07 13:13:51 +0100

    slice users async fixed

--- src/features/sliceUsers.js
+++ src/features/sliceUsers.js
@@ -1,54 +1,69 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { fetchApi } from "./fetchApi";
 
-const getAllUsers = new Promise((data) => {
-    setTimeout(() => {
-        data(fetchApi("Users"));
-    }, 200);
-})
-
 export const getApiUsers = createAsyncThunk(
     "list/fetchRooms",
-    () => {
-        return getAllUsers
-            .then((data) => data);
+    async () => {
+        return await fetchApi("Users");
+    }
+);
+
+export const createNewUser = createAsyncThunk(
+    "user/CreateUser", async (newUser) => {
+        return await newUser;
+    }
+);
+
+export const deleteUser = createAsyncThunk(
+    "user/DeleteUser", async (idUser) => {
+        return await idUser;
+    }
+);
+
+export const editUser = createAsyncThunk(
+    "user/EditUser", async (idUser) => {
+        return await idUser;
+    }
+);
+
+export const getUser = createAsyncThunk(
+    "user/GetUserDetails", async (idUser) => {
+        return await idUser;
     }
 );
 
 const initialState = {
-    users: []
+    users: [],
+    user: null
 }
 export const sliceUsers = createSlice({
     name: "users",
     initialState,
-    reducers: {
-        filterUsers: (state, action) => {
-            state.users = state.users.filter(
-                (user) => user.state === action.payload
-            );
-        },
-        addNewUser: (state, action) => {
-            state.users = [...state, action.payload]
-        },
-        deleteUser: (state, action) => {
-            state.users = state.users.filter(
-                (user) => user.id !== action.payload
-            );
-        },
-        editUser: (state, action) => {
-            state.users = state.users.map((user) => {
-                if (user.id === action.payload.id) {
-                    user = action.payload;
-                }
-                return user;
-            });
-        }
-    },
     extraReducers: (builder) => {
         builder.addCase(getApiUsers.fulfilled, (state, action) => {
             state.users = action.payload;
         }).addCase(getApiUsers.rejected, () => {
             console.error("No se han podido encontrar usuarios.");
+        });
+
+        builder.addCase(createNewUser.fulfilled, (state, action) => {
+            state.users = [...state.users, action.payload];
+        });
+
+        builder.addCase(deleteUser.fulfilled, (state, action) => {
+            state.users = state.users.filter(
+                (user) => user.id !== action.payload
+            );
+        });
+
+        builder.addCase(editUser.fulfilled, (state, action) => {
+            state.users = state.users.map((user) => {
+                return user.id === action.payload.id ? action.payload : user;
+            });
+        });
+
+        builder.addCase(getUser.fulfilled, (state, action) => {
+            state.user = state.users.find((user) => user.id === action.payload);
         });
     }
 
